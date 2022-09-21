c3331c5e6b910318b1f6ee6bebc16b320e7b8e99
rfreno <wintersbek@gmail.com>
2022-09-21 11:55:18 -0600

    boilerplate added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+
+
+
+app.listen(4000, () => {
+    console.log('Listening at port ' + port)
+})
