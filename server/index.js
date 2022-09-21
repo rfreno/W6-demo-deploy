ee13b8e40099edc6007b1bfd20c5a7adb4b292fe
rfreno <wintersbek@gmail.com>
2022-09-21 12:48:25 -0600

    end of demo

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+
+app.use(express.static(path.join(__dirname, '../client')))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
+
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../main.js'))
+// })
+
+app.listen(port, () => {
+    console.log('Listening at port ' + port)
+})
